f765adddf291f2b4a18420e5276ace8c89b0532c
Adam Argyle <atom@argyleink.com>
2013-04-14 18:16:10 -0700

    touch interactive feeds

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,21 @@
 var argyleink = {};
 
 argyleink.scriptsLoaded = function() {
-	argyleink.nav = responsiveNav("#nav", { 
-		label: 			"Menu", 	// String: Label for the navigation toggle
-		insert: 		"after", 	// String: Insert the toggle before or after the navigation
-		customToggle: 	"toggle", 	// Selector: Specify the ID of a custom toggle
-		init: function(){}, 		// Function: Init callback
-		open: function(){}, 		// Function: Open callback
-		close: function(){} 		// Function: Close callback
-	});
+	argyleink.socialCarousel = new Carousel("#social", carouselReady).init();
+	argyleink.postsCarousel = new Carousel("#posts", carouselReady).init();
+	argyleink.workCarousel = new Carousel("#work", carouselReady).init();
+
+	function carouselReady(el) {
+		$(el).removeClass('out');
+	}
 }
 
-require(["js/responsive-nav.min.js"], argyleink.scriptsLoaded);
+require(
+	[
+		  "http://raw.github.com/EightMedia/hammer.js/master/dist/jquery.hammer.min.js"
+		, "http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"
+		, "js/requestAnimationFrame.js"
+		, "js/carousel.js"
+	], 
+	argyleink.scriptsLoaded
+);
